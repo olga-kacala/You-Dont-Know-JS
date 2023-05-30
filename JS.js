bbb42ba8dd68f7b65a819d22b5afd7c859c7cd1e
olga-kacala <olga.kacala@gmail.com>
2023-05-30 10:53:40 +0200

    Variables exe

--- JS.js
+++ JS.js
@@ -4,3 +4,22 @@ alert("hello");
 
 yourName = prompt("What is your name?");
 console.log(yourName);
+
+const a = 20;
+a=a+1;
+a = a*2;
+console.log(a); //error because we can't assign again value to conts
+
+let b = 20;
+b=b+1;
+b = b*2;
+console.log(b); // 42
+
+var c = 20;
+c=c+1;
+c = c*2;
+console.log(c); //42
+
+console.log(typeof(b));
+
+"99.99" == 99.99;
